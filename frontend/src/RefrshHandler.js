f0a9d37173dfe30ac78a756c79edd66c38031dcb
Himanshu Bhandari <himanshubhandari2191@gmail.com>
2026-06-08 22:33:11 +0530

    fix: add missing import for useEffect in RefrshHandler component

--- frontend/src/RefrshHandler.js
+++ frontend/src/RefrshHandler.js
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react';
 
 function RefrshHandler({ setIsAuthenticated }) {
     const location = useLocation();
